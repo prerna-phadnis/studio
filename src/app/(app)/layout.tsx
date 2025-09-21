a0dec056cf9b23a818b15e541c99f13afb259744
Prerna Phadnis <prernaphadnis6@gmail.com>
2025-09-21 18:45:58 +0000

    add a navbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,9 +8,7 @@ export default function AppLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col bg-muted/40">
-      <div className="md:hidden">
-        <Header />
-      </div>
+      <Header />
       <div className="flex-1">
         <div className="container mx-auto grid md:grid-cols-[240px_1fr] lg:grid-cols-[280px_1fr] gap-8 py-8 px-4 md:px-8">
           <aside className="hidden md:block">
